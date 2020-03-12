fb2da7a4a836660293a4986349467e584e1992c8
ChClaude <christ.tchambila@gmail.com>
2020-03-12 10:42:03 +0200

    average calculation

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static double computeAverageGrade(double... marks) {
+        return Arrays.stream(marks).sum()/marks.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Your Average is: %.2f", computeAverageGrade(75, 75, 56, 87,
+                95, 100));
     }
 }
